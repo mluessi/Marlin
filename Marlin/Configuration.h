4bdc11250037a731c3b6b8b8dd78c6fb373d9f61
Martin Luessi <mluessi@gmail.com>
2020-06-25 16:36:43 -0700

    Correct steps per unit for X and Y

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -744,7 +744,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 114.28571428571429, 114.28571428571429, 4000, 500 }
 
 /**
  * Default Max Feed Rate (mm/s)
