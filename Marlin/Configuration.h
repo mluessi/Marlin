8eacf5fd6def696f7eccf0538eeea7ea66861ec7
Martin Luessi <mluessi@gmail.com>
2020-06-30 18:06:19 -0700

    Settings for z axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -744,7 +744,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 114.28571428571429, 114.28571428571429, 4000, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 114.28571428571429, 114.28571428571429, 400, 500 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1082,7 +1082,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
 #define INVERT_Y_DIR false
-#define INVERT_Z_DIR false
+#define INVERT_Z_DIR true
 
 // @section extruder
 
